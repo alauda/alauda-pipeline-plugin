01ee5bc00c7e89eae5bf3a33c408821f14df9d7c
jtcheng <jtcheng@alauda.io>
2018-06-08 01:13:09 +0800

    Fix bugs that findbugs plugin find
    Signed-off-by: jtcheng <jtcheng@alauda.io>

--- src/main/java/io/alauda/jenkins/plugins/pipeline/dsl/notification/models/NotificationPayload.java
+++ src/main/java/io/alauda/jenkins/plugins/pipeline/dsl/notification/models/NotificationPayload.java
@@ -8,118 +8,269 @@ import io.alauda.jenkins.plugins.pipeline.utils.DateSerializer;
 import java.util.Date;
 
 public class NotificationPayload {
+    public String getTemplateType() {
+        return templateType;
+    }
+
+    public void setTemplateType(String templateType) {
+        this.templateType = templateType;
+    }
+
+    public NotificationPayloadData getData() {
+        return data;
+    }
+
+    public void setData(NotificationPayloadData data) {
+        this.data = data;
+    }
+
     @JsonProperty("template_type")
-    public String TemplateType="jenkins";
+    private String templateType = "jenkins";
     @JsonProperty("data")
-    public data data;
+    public NotificationPayloadData data;
 
 
-    public NotificationPayload(){
-        this.data = new data();
+    public NotificationPayload() {
+        this.data = new NotificationPayloadData();
         this.data.time = new Date();
     }
 
-    public NotificationPayload setSubject(String subject){
+    public NotificationPayload setSubject(String subject) {
         this.data.subject = subject;
         return this;
     }
 
-    public NotificationPayload setContent(String content){
+    public NotificationPayload setContent(String content) {
         this.data.content = content;
         return this;
     }
 
-    public NotificationPayload setCauseText(String cause){
+    public NotificationPayload setCauseText(String cause) {
         this.data.payload.causeText = cause;
         return this;
     }
 
-    public NotificationPayload setRepo(String repo){
+    public NotificationPayload setRepo(String repo) {
         this.data.payload.repo = repo;
         return this;
     }
 
-    public NotificationPayload setRepoBranch(String branch){
+    public NotificationPayload setRepoBranch(String branch) {
         this.data.payload.repoBranch = branch;
         return this;
     }
 
-    public NotificationPayload setRepoVersion(String version){
+    public NotificationPayload setRepoVersion(String version) {
         this.data.payload.repoVersion = version;
         return this;
     }
 
-    public NotificationPayload setJobURL(String jobURL){
+    public NotificationPayload setJobURL(String jobURL) {
         this.data.payload.jobURL = jobURL;
         return this;
     }
 
-    public  NotificationPayload setStartedAt(Date startedAt){
+    public NotificationPayload setStartedAt(Date startedAt) {
         this.data.payload.startedAt = startedAt;
         return this;
     }
 
-    public NotificationPayload setDuration(String duration){
+    public NotificationPayload setDuration(String duration) {
         this.data.payload.duration = duration;
         return this;
     }
 
-    public NotificationPayload setEndedAt(Date endedAt){
+    public NotificationPayload setEndedAt(Date endedAt) {
         this.data.payload.endAt = endedAt;
-        return  this;
+        return this;
     }
 
-    public NotificationPayload setStatus(String status){
+    public NotificationPayload setStatus(String status) {
         this.data.payload.status = status;
         return this;
     }
 
-    public NotificationPayload setBody(String body){
+    public NotificationPayload setBody(String body) {
         this.data.payload.body = body;
         return this;
     }
 
-    public static class data {
-        public String subject;
+    public static class NotificationPayloadData {
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public Date getTime() {
+            if (this.time != null) {
+                return new Date(this.time.getTime());
+            }
+            return null;
+        }
+
+        public void setTime(Date time) {
+            if (time != null){
+                this.time = (Date)time.clone();
+            }else{
+                this.time = null;
+            }
+
+        }
+
+        public Payload getPayload() {
+            return payload;
+        }
+
+        public void setPayload(Payload payload) {
+            this.payload = payload;
+        }
+
+        private String subject;
         @JsonProperty("content")
-        public String content="";
+        private String content = "";
 
         @JsonSerialize(using = DateSerializer.class)
         @JsonInclude(JsonInclude.Include.NON_NULL)
-        public Date time;
+        private Date time;
 
         @JsonProperty("payload")
-        public payload payload;
-        public data(){
-            this.payload = new payload();
+        private Payload payload;
+
+        public NotificationPayloadData() {
+            this.payload = new Payload();
         }
     }
 
-    public static class payload{
+    public static class Payload {
+        public String getCauseText() {
+            return causeText;
+        }
+
+        public void setCauseText(String causeText) {
+            this.causeText = causeText;
+        }
+
+        public String getRepo() {
+            return repo;
+        }
+
+        public void setRepo(String repo) {
+            this.repo = repo;
+        }
+
+        public String getRepoBranch() {
+            return repoBranch;
+        }
+
+        public void setRepoBranch(String repoBranch) {
+            this.repoBranch = repoBranch;
+        }
+
+        public String getRepoVersion() {
+            return repoVersion;
+        }
+
+        public void setRepoVersion(String repoVersion) {
+            this.repoVersion = repoVersion;
+        }
+
+        public String getJobURL() {
+            return jobURL;
+        }
+
+        public void setJobURL(String jobURL) {
+            this.jobURL = jobURL;
+        }
+
+        public Date getStartedAt() {
+            if(this.startedAt!=null){
+                return new Date(this.startedAt.getTime());
+            }
+            return null;
+        }
+
+        public void setStartedAt(Date startedAt) {
+            if(startedAt!=null){
+                this.startedAt = (Date) startedAt.clone();
+            }else{
+                this.startedAt = null;
+            }
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public void setDuration(String duration) {
+            this.duration = duration;
+        }
+
+        public Date getEndAt() {
+            if (endAt != null) {
+                return new Date(this.endAt.getTime());
+            }
+            return null;
+        }
+
+        public void setEndAt(Date endAt) {
+            if (endAt != null){
+                this.endAt = (Date) endAt.clone();
+            }else{
+                this.endAt = null;
+            }
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public void setBody(String body) {
+            this.body = body;
+        }
+
         @JsonProperty("cause_text")
-        public String causeText;
+        private String causeText;
         public String repo;
         @JsonProperty("repo_branch")
-        public String repoBranch;
+        private String repoBranch;
         @JsonProperty("repo_version")
-        public String repoVersion;
+        private String repoVersion;
         @JsonProperty("job_url")
-        public String jobURL;
+        private String jobURL;
 
         @JsonProperty("started_at")
         @JsonInclude(JsonInclude.Include.NON_NULL)
         @JsonSerialize(using = DateSerializer.class)
-        public Date startedAt;
+        private Date startedAt;
 
         @JsonProperty("duration")
-        public String duration;
+        private String duration;
 
         @JsonProperty("ended_at")
         @JsonInclude(JsonInclude.Include.NON_NULL)
         @JsonSerialize(using = DateSerializer.class)
-        public Date endAt;
+        private Date endAt;
 
-        public String status;
-        public String body;
+        private String status;
+        private String body;
     }
 }
